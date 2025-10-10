e41af10bdb1cbb6def890f5cd7a3594eebc9a0f9
erysdren <contact@erysdren.me>
2025-10-10 17:18:48 -0500

    initial x11 graphics import
    Co-authored-by: Howard Chu <hyc@symas.com>
    Co-authored-by: erysdren <contact@erysdren.me>

---
+++ src/libbitblit/x11-backend/bit_bytescr.c
@@ -0,0 +1,31 @@
+/* Copyright (c) 2014 Howard Chu, hyc@symas.com
+ *                            All Rights Reserved
+ *       Permission is granted to copy or use this program, EXCEPT that it
+ *       may not be sold for profit, the copyright notice must be reproduced
+ *       on copies, and credit should be given to the author where it is due.
+ *       THE AUTHOR MAKES NO WARRANTY AND ACCEPTS NO LIABILITY FOR THIS PROGRAM.
+ */
+
+#include "bitmap.h"
+#include <mgr/share.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*{{{  bit_bytescroll -- scroll a byte-aligned region */
+void bit_bytescroll(BITMAP *map, int x, int y, int wide, int high, int delta)
+{
+	long byteswide = map->primary->wide;
+	char *dst = ((char *)(map->data)) + y*byteswide + x;
+	char *src = dst + delta*byteswide;
+	long ncount = high - delta;
+
+#ifdef MOVIE
+	log_bytescroll(map,x,y,wide,high,delta);
+#endif
+	while (ncount--) {
+		memcpy(dst, src, wide);
+		dst += byteswide;
+		src += byteswide;
+	}
+}
+/*}}}  */
